f7ee5b7e3849f409f48014afc7d8e712df6682e2
kirtisingh-001 <kirti.22scse1012550@galgotiasuniversity.edu.in>
2026-04-27 10:21:49 +0530

    Initial commit - Sales Data Analysis Project

--- src/visualize.py
+++ src/visualize.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 # Load data
-df = pd.read_csv("data/sales_data.csv")
+df = pd.read_csv("../sales_data.csv")
 
-# Create graph
+# Create graph (FIXED LINE)
 df.groupby("Region")["Sales"].sum().plot(kind="bar")
 
 plt.title("Sales by Region")
 
-# SAVE GRAPH (IMPORTANT LINE)
-plt.savefig("outputs/plots/sales_by_region.png")
+# Create folder if not exists
+os.makedirs("../outputs/plots", exist_ok=True)
+
+# Save graph
+plt.savefig("../outputs/plots/sales_by_region.png")
 
 # Show graph
 plt.show()
